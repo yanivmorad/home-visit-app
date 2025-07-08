9298b0938ebef0f5d360c9d5e8d03a69bccf4f90
DESKTOP-1PK25N3\yc <yanivmorad10@gmail.com>
2025-07-08 16:40:19 +0300

    update

--- src/api/childrenApi.js
+++ src/api/childrenApi.js
@@ -2,8 +2,8 @@
 
 // import { clientFetch } from "./client";
 
-const BASE = "http://localhost:4000/api/children";
-// const BASE = "https://home-visit-backend.onrender.com/api/children";
+// const BASE = "http://localhost:4000/api/children";
+const BASE = "https://home-visit-backend.onrender.com/api/children";
 
 export async function fetchChildren() {
   const res = await fetch(BASE);
